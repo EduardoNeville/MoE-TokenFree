aa45b823fe09a91b3480a1b38aee1f1360db6e1c
EduardoNeville <eduardoneville82@gmail.com>
2024-05-15 10:14:29 +0200

    added openweb training

--- models/tokenizer.py
+++ models/tokenizer.py
@@ -1,27 +1,38 @@
-import transformers import AutoTokenizer
+from dataclasses import dataclass, field
+from transformers import AutoTokenizer
 import tiktoken
-import torch
+from torch import Tensor, as_tensor
 import numpy as np
+@dataclass
+class Tokenizer:
+    tokenizer_name: str
+    tokenizer = field(init=False)
 
-class Tokenizer():
-    def __init__(self, tokenizer) -> None:
-        self.tokenizer = tokenizer_selector(self, tokenizer)
-        pass
+    def __post_init__(self):
+        self.tokenizer = self.tokenizer_selector(self.tokenizer_name)
 
-    def huggingface_tokenization(self, input_text, ):
+    def tokenizer_selector(self, tokenizer_name: str):
+        if tokenizer_name == "google/byt5-base":
+            return AutoTokenizer.from_pretrained("google/byt5-base")
+        elif tokenizer_name == "tiktoken":
+            return tiktoken.get_encoding("gpt2")
+        else:
+            raise ValueError("Invalid tokenizer name")
+
+    def tokenize(self, input_text: str)-> Tensor:
+        if isinstance(self.tokenizer, AutoTokenizer):
+            return self.huggingface_tokenization(input_text)
+        elif isinstance(self.tokenizer, tiktoken.Encoding):
+            return self.tiktoken_tokenization(input_text)
+        else:
+            raise TypeError("Tokenizer type not supported")
+
+    def huggingface_tokenization(self, input_text: str)-> Tensor:
         inputs = self.tokenizer(input_text, return_tensors="pt")["input_ids"]
-
-        
-    def tiktoken_tokenization(self, input_text):
-        inputs = self.tokenizer.encode_ordinary(input_text)
-        inputs = torch.as_tensor(np.array([inputs]))
         return inputs
 
-    def tokenizer_selector(self, tokenizer):
-        match tokenizer:
-            case "google/byt5-base":
-                return AutoTokenizer.from_pretrained("google/byt5-base")
-            case "tiktoken":
-                return tiktoken.get_encoding("gpt2")
-            case _:
-                raise ValueError("Invalid tokenizer")
+    def tiktoken_tokenization(self, input_text: str):
+        inputs = self.tokenizer.encode_ordinary(input_text)
+        inputs = as_tensor(np.array([inputs]))
+        return inputs
+
---
+++ train_models/openweb_train_exp4_top_2.py
@@ -0,0 +1,120 @@
+import argparse
+import os
+from pathlib import Path
+import subprocess
+import time
+from argparse import Namespace
+import pathlib
+import sys
+
+ # setting path
+#sys.path.append(pathlib.Path().parent.absolute())
+
+# Base shell command
+#change names of train and val dataset
+#Removed 
+#--attention_type='standard' \
+#--out_dir='wikitexts-byt5-std-'$wandb_name \
+shell_cmd = r"""python train.py \
+--dataset='openwebtext' \
+--init_from='scratch' \
+--out_dir='openweb-byt5-exp-4-top-2' \
+--eval_interval=5 \
+--eval_iters=40 \
+--is_cached=False \
+--wandb_log=True \
+--wandb_project='ec-llm' \
+--wandb_run_name=$wandb_name \
+--always_save_checkpoint=True \
+--batch_size=8 \
+--gradient_accumulation_steps=16 \
+--max_iters=$num_iters \
+--lr_decay_iters=$num_iters \
+--warmup_iters=$warmup_iters \
+--learning_rate=$lr \
+--min_lr=$min_lr \
+--weight_decay=$wd \
+--decay_lr=$decay_lr \
+--device='cuda' \
+--compile=True \
+--compute_grad_memory=True \
+--mlp_type='moe' \
+--router_type='standard' \
+--lin_type='standard' \
+--lora_rank=0 \
+--lora_alpha=32.0 \
+--expert_num=4 \
+--base_seed_offset=$seed \
+--noise_type='normal' \
+--load_balancing=True \
+--moe_lin_type='standard' \
+--straight_through=False \
+--global_routing=False \
+--topk_exp=2 \
+--is_per_token=True \
+--router_lr_scaling=0.0 \
+--router_depth=1 \
+--load_balancing_lambda=0.01 \
+--attention_type='standard' \
+"""
+
+min_lk=6e-5
+learning_rate=6e-4
+min_lr=6e-5
+weight_decay=1e-1
+decay_lr=True
+
+# Configure runs
+def main():
+    base_dir = os.getcwd()
+    seeds = [ 2 ]
+    num_iters = [ 1600 ]
+    # New name for wandb
+    train_name = f"openwebtext_byt5_exp_4_top_2_numIter_{num_iters}"
+
+    idx = 0
+    for seed in seeds:
+        for num_iter in num_iters:
+            if do_run(idx):
+                print(f"===== Running iteration={idx} {learning_rate=:0.3e} {weight_decay=:0.3e} =====")
+                current_time = str(time.time())
+                subprocess.run(
+                    ['/bin/bash', '-c', f"echo {shell_cmd} \n {shell_cmd}"],
+                    env={k: str(v) for k, v in dict(
+                        # The keys and values need to be strings
+                        **os.environ,  # Inherit original variables (e.g. conda)
+                        wandb_name=train_name,
+                        num_iters=num_iter,
+                        lr=f"{learning_rate:0.6}",
+                        min_lr=f"{min_lr:0.6}",
+                        wd=f"{weight_decay:0.6}",
+                        iter=idx,
+                        seed=seed,
+                        decay_lr=decay_lr,
+                        warmup_iters=int(0.03 * num_iter)
+                    ).items()}
+            )
+            idx += 1
+
+if __name__ == "__main__":
+    # Arguments to run a subset of iterations (e.g. for parallelization or infra failures)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--range", nargs=2, type=int, default=None,
+        help="Run a subrange of the iterations from A to B (inclusive)",
+    )
+    parser.add_argument(
+        "--iters", nargs='+', type=int, default=None,
+        help="Run the provided subset of iterations",
+    )
+    args = parser.parse_args()
+
+    assert not (args.range and args.iters)
+    if args.range:
+        do_run = lambda idx: args.range[0] <= idx <= args.range[1]
+    elif args.iters:
+        do_run = lambda idx: idx in args.iters
+    else:
+        do_run = lambda idx: True
+
+    main()
