06d7dd3092e22f58e81d240e53ca33f81b125dab
EduardoNeville <eduardoneville82@gmail.com>
2024-06-03 21:36:04 +0200

    modifs to arc for no , in csv

--- evals/arc_easy/arc_eval.py
+++ evals/arc_easy/arc_eval.py
@@ -3,17 +3,14 @@ import csv
 # Load the dataset from Hugging Face
 dataset = load_dataset("ibragim-bad/arc_easy")
 # Open a CSV file for writing
-with open('evals/arc_easy/data/test/arc_easy.csv', mode='w', newline='') as file:
+with open('evals/arc_easy/data/test/arc_easy_test.csv', mode='w', newline='') as file:
     writer = csv.writer(file)
-    
-    # Write the header
-    writer.writerow(['question', 'choice_A', 'choice_B', 'choice_C', 'choice_D', 'answerKey'])
     
     # Iterate over the dataset rows
     for split_name, split_data in dataset.items():
         print(f"Parsing {split_name}... \n")
         for row in split_data:
-            question = row['question']
+            question = row['question'].replace(","," ")
             choices = row['choices']['text']
             answerKey = row['answerKey']
             
