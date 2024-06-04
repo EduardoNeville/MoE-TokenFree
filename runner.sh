b10e99cb1d9f19dd11d672d8c568b1b7a2ec0619
EduardoNeville <eduardoneville82@gmail.com>
2024-06-04 14:12:26 +0200

    runner preped for arc_easy

--- runner.sh
+++ runner.sh
@@ -9,7 +9,7 @@ echo "::::::::::::::::::::::::::::::::::::::::::::::::"
 echo "Running MMLU: BYT5 STANDARD"
 echo "::::::::::::::::::::::::::::::::::::::::::::::::"
 echo "::::::::::::::::::::::::::::::::::::::::::::::::"
-./evals/mmlu/mmlu/mmlu_script.sh byt5 std mmlu
+./evals/mmlu/mmlu/mmlu_script.sh byt5 std arc_easy
 echo "----------- DONE -----------"
 
 echo "::::::::::::::::::::::::::::::::::::::::::::::::"
@@ -17,7 +17,7 @@ echo "::::::::::::::::::::::::::::::::::::::::::::::::"
 echo "Running MMLU: BYT5 EXP_4_TOP_2"
 echo "::::::::::::::::::::::::::::::::::::::::::::::::"
 echo "::::::::::::::::::::::::::::::::::::::::::::::::"
-./evals/mmlu/mmlu/mmlu_script.sh byt5 exp_4_top_2 mmlu
+./evals/mmlu/mmlu/mmlu_script.sh byt5 exp_4_top_2 arc_easy
 echo "----------- DONE -----------"
 
 echo "::::::::::::::::::::::::::::::::::::::::::::::::"
@@ -25,7 +25,7 @@ echo "::::::::::::::::::::::::::::::::::::::::::::::::"
 echo "Running MMLU: BYT5 EXP_4_TOP_1"
 echo "::::::::::::::::::::::::::::::::::::::::::::::::"
 echo "::::::::::::::::::::::::::::::::::::::::::::::::"
-./evals/mmlu/mmlu/mmlu_script.sh byt5 exp_4_top_1 mmlu
+./evals/mmlu/mmlu/mmlu_script.sh byt5 exp_4_top_1 arc_easy
 echo "----------- DONE -----------"
 
 echo "::::::::::::::::::::::::::::::::::::::::::::::::"
@@ -39,7 +39,7 @@ echo "::::::::::::::::::::::::::::::::::::::::::::::::"
 echo "Running MMLU: GPT STANDARD"
 echo "::::::::::::::::::::::::::::::::::::::::::::::::"
 echo "::::::::::::::::::::::::::::::::::::::::::::::::"
-./evals/mmlu/mmlu/mmlu_script.sh gpt std mmlu
+./evals/mmlu/mmlu/mmlu_script.sh gpt std arc_easy
 echo "----------- DONE -----------"
 
 echo "::::::::::::::::::::::::::::::::::::::::::::::::"
@@ -47,7 +47,7 @@ echo "::::::::::::::::::::::::::::::::::::::::::::::::"
 echo "Running MMLU: GPT EXP_4_TOP_2"
 echo "::::::::::::::::::::::::::::::::::::::::::::::::"
 echo "::::::::::::::::::::::::::::::::::::::::::::::::"
-./evals/mmlu/mmlu/mmlu_script.sh gpt exp_4_top_2 mmlu
+./evals/mmlu/mmlu/mmlu_script.sh gpt exp_4_top_2 arc_easy
 echo "----------- DONE -----------"
 
 echo "::::::::::::::::::::::::::::::::::::::::::::::::"
@@ -55,5 +55,5 @@ echo "::::::::::::::::::::::::::::::::::::::::::::::::"
 echo "Running MMLU: GPT EXP_4_TOP_1"
 echo "::::::::::::::::::::::::::::::::::::::::::::::::"
 echo "::::::::::::::::::::::::::::::::::::::::::::::::"
-./evals/mmlu/mmlu/mmlu_script.sh byt5 exp_4_top_1 mmlu
+./evals/mmlu/mmlu/mmlu_script.sh byt5 exp_4_top_1 arc_easy
 echo "----------- DONE -----------"
